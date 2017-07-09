816621beec34fccf3242744bd0278b1a5e6ddf46
WooKyoung Noh <wookay.noh@gmail.com>
2017-07-09 18:34:26 +0900

    pyimport bpy_extras, bgl, mathutils

--- src/BlenderPlot.jl
+++ src/BlenderPlot.jl
@@ -3,12 +3,15 @@ module BlenderPlot
 using PyCall
 
 if endswith(unsafe_string(Base.JLOptions().julia_bin), "julia")
-    warn("Please run on the Blender Python Console")
+    warn("Please run on the Blender Julia Console")
 else
-    include("pycall/util.jl")
-    export bpy
+    export bpy, bpy_extras, bgl, mathutils
     @pyimport bpy
+    @pyimport bpy_extras
+    @pyimport bgl
+    @pyimport mathutils
     include("pycall/plot.jl")
+    include("pycall/util.jl")
 end
 
 end # module
